f79528fe8c0a84fec316a463f7b8863563786da7
Kamil Pokora <kamilpokora97@gmail.com>
2018-02-22 15:21:47 +0100

    Delete file.

--- main.py
+++
@@ -1,6 +0,0 @@
-from LeagueResultPredictor import LeagueResultPredictor
-
-league=LeagueResultPredictor("D1",5)
-league.trainClassifiers()
-classifier=league.getLogisticRegressionClassifier()
-league.getProbabilitiesForUpcomingMatches(classifier)
